56bc28a1db8249808c7c4cb103abddfa9122d00d
Fabio Pineda <fabiopineda97@gmail.com>
2025-07-30 19:38:05 -0400

    Small performance optimizations on Script/Fonts loading

--- public/emailjs.js
+++ public/emailjs.js
@@ -1,3 +1,18 @@
-(function () {
-    emailjs.init('Bi61EkDhk1cn1Hj4m');
-  })();
+(function() {
+  // Wait for EmailJS library to be available
+  function initEmailJS() {
+    if (typeof emailjs !== 'undefined') {
+      emailjs.init('Bi61EkDhk1cn1Hj4m');
+    } else {
+      // Retry if EmailJS isn't loaded yet
+      setTimeout(initEmailJS, 100);
+    }
+  }
+  
+  // Start initialization when DOM is ready
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initEmailJS);
+  } else {
+    initEmailJS();
+  }
+})();
